3ab542bd729d017bf54041243942ea174f1f2fd6
DESKTOP-T68U191\Gebruiker <jesse.klijn@hva.nl>
2018-01-20 14:55:44 +0900

    Project Initial push.

---
+++ JapanPrototype/Assets/Script/ButtonPressMethods.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPressMethods : MonoBehaviour {
+
+    public AudioClip[] clips;
+    public AudioSource audioSource;
+
+	public void PlayAudio(int index)
+    {
+        audioSource.clip = clips[index];
+        audioSource.Play();
+    }
+  
+}
