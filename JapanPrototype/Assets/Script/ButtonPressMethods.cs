a996d28823a10491ead1358abcdc2f9e2f859515
Atakan Kaya <demertknight@gmail.com>
2018-01-22 00:09:52 +0900

    all asked screens created... buttons are bugged af, and doesn't call the method onclick ...wtf?!

--- JapanPrototype/Assets/Script/ButtonPressMethods.cs
+++ JapanPrototype/Assets/Script/ButtonPressMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonPressMethods : MonoBehaviour {
 
@@ -11,6 +12,39 @@ public class ButtonPressMethods : MonoBehaviour {
     {
         audioSource.clip = clips[index];
         audioSource.Play();
+    }
+
+    public static void SwitchScene(int i)
+    {
+        if(i == 2)
+        {
+            SceneManager.LoadScene("_AppMainScreen", LoadSceneMode.Additive);
+        }
+        
+        else if(i == 3)
+        {
+            SceneManager.LoadScene("_HomeMenu", LoadSceneMode.Additive);
+        }
+        else if(i == 4)
+        {
+            SceneManager.LoadScene("_Profile", LoadSceneMode.Additive);
+        }
+        else if(i == 5)
+        {
+            SceneManager.LoadScene("_Level", LoadSceneMode.Additive);
+        }
+        else if(i == 6)
+        {
+            SceneManager.LoadScene("_ResearchMenu", LoadSceneMode.Additive);
+        }
+        else if(i == 7)
+        {
+            SceneManager.LoadScene("_UniverseMenu", LoadSceneMode.Additive);
+        }
+        else if(i == 8)
+        {
+            SceneManager.LoadScene("_Topic", LoadSceneMode.Additive);
+        }
     }
   
 }
